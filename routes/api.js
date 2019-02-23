191e89b7ef610ee1052f39885f3c00c3d1bc5acf
Morgan Galvin <morgan@ludwigthepig.com>
2019-02-24 12:21:42 +1300

    Routed thread HTTP requests

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,15 @@
-/*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
-
 'use strict';
-
 var expect = require('chai').expect;
+const Thread = require('../controllers/thread-handler.js');
+
+const thread = new Thread;
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+    .get(thread.getThreads)
+    .post(thread.addThread)
+    .delete(thread.deleteThread);
     
   app.route('/api/replies/:board');
 

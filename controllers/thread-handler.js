caf2fc944e98522165f54cac644bafcb273d8914
Morgan Galvin <morgan@ludwigthepig.com>
2019-02-24 12:22:40 +1300

    Structured thread handler

---
+++ controllers/thread-handler.js
@@ -0,0 +1,14 @@
+const Thread = require('../models/thread.js');
+
+function ControllerHandler(){
+  this.getThreads = (req, res)=>{
+  }
+  
+  this.addThread = (req, res)=>{
+  }
+  
+  this.deleteThread = (req, res)=>{
+  }
+}
+
+module.exports = ControllerHandler;

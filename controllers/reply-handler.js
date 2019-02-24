36c4dad905f0206fc0927ff56a878bfc4a5bc885
Morgan Galvin <morgan@ludwigthepig.com>
2019-02-25 11:40:39 +1300

    Can now get and add comments/replies
    Need to be consistent with comment or reply term in code

--- controllers/reply-handler.js
+++ controllers/reply-handler.js
@@ -2,10 +2,23 @@ const Thread = require('../models/thread-model.js');
 
 function ReplyHandler(){
   this.getReplies = (req, res)=>{
+    const id = req.params.board;
+    Thread.findById(id, (err, doc)=>{
+      if (err) console.log(err);
+      res.json(doc);
+    })
   };
   
-  this.postReply = (req, res)=>{
-    
+  this.addReply = (req, res)=>{
+    console.log(req.body);
+    const reply = req.body.reply;
+    const id = req.body.id;
+    Thread.findById(id, (err, doc)=>{
+      if (err) console.log(err);
+      doc.comments.push(reply);
+      doc.save()
+        .then(console.log(doc));
+    })
   };
   
   this.deleteReply = (req, res)=>{
---
+++ public/js/replies.js
@@ -0,0 +1,79 @@
+const url = location.pathname.slice(3).toString().split('/');
+const board = url[0];
+const id = url[1];
+
+let thread = {};
+const http = {
+  getter: ()=>{
+    return fetch(`/api/replies/${id}`)
+      .then(response => response.json())
+      .then(data => thread = data)
+      .then(_=> renderer())
+      .catch(err => console.log(err));
+  },
+  poster: (reply)=>{
+    const options = {
+      method: "POST",
+      body: reply,
+      headers: {'Content-Type': 'application/json'}
+    };
+    return fetch(`/api/replies/${id}`, options)
+      .then(response => response)
+      .then(data => data)
+      .catch(err => console.log(err));
+  },
+  
+  deleter: (thread)=>{
+    const options = {
+      method: "POST",
+      body: thread,
+      headers: {'Content-Type': 'application/json'}
+    };
+    return fetch(`/api/replies/${id}`)
+  }
+}//end http
+const renderer = ()=>{
+  const boardLink = document.createElement('h2');
+  boardLink.innerText = `Go back to ${board}`;
+  boardLink.setAttribute('onclick', `location.href='/b/${board}'`);
+  document.getElementById('header').appendChild(boardLink);
+  
+  const threadText = document.createElement('p');
+  threadText.innerText = thread.threadText;
+  document.getElementById('thread').appendChild(threadText);
+  
+  if (thread.comments.length !== 0){
+    const comments = thread.comments;
+    const commentList = document.createElement('ul');
+    for (let i = 0; i < comments.length; i++){
+      const li = document.createElement('li');
+      li.innerText = comments[i];
+      commentList.appendChild(li);
+    }
+    document.getElementById('replyNode').appendChild(commentList);
+  }
+};
+
+const eventListeners = ()=>{
+  const dom = {
+    form: document.getElementById('replyForm'),
+    reply: document.getElementsByName('text')[0],
+  }
+  dom.form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const request = {
+      "reply": dom.reply.value,
+      "id": id
+    }
+    http.poster(JSON.stringify(request));
+  })
+};
+
+
+const init = ()=>{
+  http.getter();
+  eventListeners();
+
+};
+
+document.onload = init();

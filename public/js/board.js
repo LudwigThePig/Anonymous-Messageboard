dcfc2d6eeb6c28d53075ca16ef6022c21140aca9
Morgan Galvin <morgan@ludwigthepig.com>
2019-02-26 07:47:46 +1300

    Added reload after form submission and renamed files
    renamed thread.js to boards.js and replies.js to thread.js

---
+++ public/js/board.js
@@ -0,0 +1,88 @@
+const boardName = location.pathname.slice(3).toString();
+let threads = [];
+const http = {
+  getter: ()=>{
+    return fetch(`/api/threads/${boardName}`)
+      .then(response => response.json())
+      .then(data => threads = data)
+      .then(_=> renderer())
+      .catch(err => console.log(err));
+  },
+  poster: (thread)=>{
+    const options = {
+      method: "POST",
+      body: thread,
+      headers: {'Content-Type': 'application/json'}
+    }
+    return fetch(`/api/threads/${boardName}`, options)
+      .then(response => response)
+      .then(data => data)
+      .catch(err => console.log(err));
+  },
+  deleter: (thread)=>{
+    const options = {
+      method: "POST",
+      body: thread,
+      headers: {'Content-Type': 'application/json'}
+    }
+    return fetch(`/api/threads/${boardName}`)
+  }
+}//end http
+const renderer = ()=>{  
+  const domNode = document.getElementById('threads');
+  for (let i = 0; i < threads.length; i++){
+    const div = document.createElement('div');
+    const threadText = document.createElement('h3');
+    const date = document.createElement('span');
+    const deleteLink = document.createElement('span');
+    const replyCount = document.createElement('span');
+    
+    if (threads[i].threadText.length > 100){ //Display only some of the text
+      threadText.innerText = threads[i].threadText.slice(0, 100) + "...";
+    } else {
+      threadText.innerText = threads[i].threadText
+    }
+    date.innerText = threads[i].dateCreated;
+    deleteLink.innerText = 'Delete Thread';
+    replyCount.innerText = `${threads[i].comments.length} replies`
+    
+    div.setAttribute('class', 'threadDiv');
+    div.setAttribute('onclick', `location.href='/b/${threads[i].board}/${threads[i]._id}'`)
+    deleteLink.setAttribute('class', 'deleteLink');
+    deleteLink.setAttribute('id', threads[i]._id)
+    
+    div.appendChild(threadText);
+    div.appendChild(date);
+    div.appendChild(replyCount);
+    div.appendChild(deleteLink);
+    domNode.appendChild(div)
+  }
+  
+};
+
+const eventListeners = ()=>{
+  const dom = {
+    form: document.getElementById('newThread'),
+    textArea: document.getElementsByName('text')[0],
+    deleteKey: document.getElementsByName('deleteKey')[0]
+  }
+  dom.form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const request = {
+      "threadText": dom.textArea.value,
+      "deleteKey": dom.deleteKey.value
+    }
+    http.poster(JSON.stringify(request));
+    location.reload();
+
+  })
+};
+
+const init = ()=>{
+  http.getter();
+  const boardNameFormated = boardName.slice(0,1).toUpperCase() + boardName.slice(1);
+  document.getElementById('boardName').innerText = `${boardNameFormated} Board`;
+  eventListeners();
+};
+
+document.onload = init();
--- public/js/thread.js
+++ public/js/thread.js
@@ -1,86 +1,80 @@
-const boardName = location.pathname.slice(3).toString();
-let threads = [];
+const url = location.pathname.slice(3).toString().split('/');
+const board = url[0];
+const id = url[1];
+
+let thread = {};
 const http = {
   getter: ()=>{
-    return fetch(`/api/threads/${boardName}`)
+    return fetch(`/api/replies/${id}`)
       .then(response => response.json())
-      .then(data => threads = data)
+      .then(data => thread = data)
       .then(_=> renderer())
       .catch(err => console.log(err));
   },
-  poster: (thread)=>{
+  poster: (reply)=>{
     const options = {
       method: "POST",
-      body: thread,
+      body: reply,
       headers: {'Content-Type': 'application/json'}
-    }
-    return fetch(`/api/threads/${boardName}`, options)
+    };
+    return fetch(`/api/replies/${id}`, options)
       .then(response => response)
       .then(data => data)
       .catch(err => console.log(err));
   },
+  
   deleter: (thread)=>{
     const options = {
       method: "POST",
       body: thread,
       headers: {'Content-Type': 'application/json'}
-    }
-    return fetch(`/api/threads/${boardName}`)
+    };
+    return fetch(`/api/replies/${id}`)
   }
 }//end http
-const renderer = ()=>{  
-  const domNode = document.getElementById('threads');
-  for (let i = 0; i < threads.length; i++){
-    const div = document.createElement('div');
-    const threadText = document.createElement('h3');
-    const date = document.createElement('span');
-    const deleteLink = document.createElement('span');
-    const replyCount = document.createElement('span');
-    
-    if (threads[i].threadText.length > 100){ //Display only some of the text
-      threadText.innerText = threads[i].threadText.slice(0, 100) + "...";
-    } else {
-      threadText.innerText = threads[i].threadText
-    }
-    date.innerText = threads[i].dateCreated;
-    deleteLink.innerText = 'Delete Thread';
-    replyCount.innerText = `${threads[i].comments.length} replies`
-    
-    div.setAttribute('class', 'threadDiv');
-    div.setAttribute('onclick', `location.href='/b/${threads[i].board}/${threads[i]._id}'`)
-    deleteLink.setAttribute('class', 'deleteLink');
-    deleteLink.setAttribute('id', threads[i]._id)
-    
-    div.appendChild(threadText);
-    div.appendChild(date);
-    div.appendChild(replyCount);
-    div.appendChild(deleteLink);
-    domNode.appendChild(div)
-  }
+const renderer = ()=>{
+  const boardLink = document.createElement('h2');
+  boardLink.innerText = `Go back to ${board}`;
+  boardLink.setAttribute('onclick', `location.href='/b/${board}'`);
+  document.getElementById('header').appendChild(boardLink);
   
+  const threadText = document.createElement('p');
+  threadText.innerText = thread.threadText;
+  document.getElementById('thread').appendChild(threadText);
+  
+  if (thread.comments.length !== 0){
+    const comments = thread.comments;
+    const commentList = document.createElement('ul');
+    for (let i = 0; i < comments.length; i++){
+      const li = document.createElement('li');
+      li.innerText = comments[i];
+      commentList.appendChild(li);
+    }
+    document.getElementById('replyNode').appendChild(commentList);
+  }
 };
 
 const eventListeners = ()=>{
   const dom = {
-    form: document.getElementById('newThread'),
-    textArea: document.getElementsByName('text')[0],
-    deleteKey: document.getElementsByName('deleteKey')[0]
+    form: document.getElementById('replyForm'),
+    reply: document.getElementsByName('text')[0],
   }
   dom.form.addEventListener('submit', (e)=>{
     e.preventDefault();
     const request = {
-      "threadText": dom.textArea.value,
-      "deleteKey": dom.deleteKey.value
+      "reply": dom.reply.value,
+      "id": id
     }
     http.poster(JSON.stringify(request));
+    location.reload();
   })
 };
 
+
 const init = ()=>{
   http.getter();
-  const boardNameFormated = boardName.slice(0,1).toUpperCase() + boardName.slice(1);
-  document.getElementById('boardName').innerText = `${boardNameFormated} Board`;
   eventListeners();
+
 };
 
 document.onload = init();

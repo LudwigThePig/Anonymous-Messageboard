272ffe4809501d40a9bca50bb1a9f835f37a664c
Morgan Galvin <morgan@ludwigthepig.com>
2019-03-10 17:40:17 +1300

    Added routes/auth boilerplate

---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+const session = require('express-session');
+const mongo = require('mongodb').MongoClient;
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user-model.js');
+
+module.exports = function (app, db) {
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      User.findOne({ username: username }, function (err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (!user.verifyPassword(password)) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
+}
